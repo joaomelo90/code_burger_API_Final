703c987446ff25b91bfc7b885cd2ae31f23f871f
joaomelo90 <joaopaulomoreiramelo@gmail.com>
2023-12-15 19:42:38 -0300

    update de Category

--- src/app/controller/CategoryController.js
+++ src/app/controller/CategoryController.js
@@ -43,6 +43,48 @@ class CategoryController {
 
     return response.json(category)
   }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { name } = request.body
+
+    const { id } = request.params
+
+    const category = await Category.findByPk(id)
+
+    if (!category) {
+      return response
+        .status(401)
+        .json({ error: 'Make sure your category id is correct' })
+    }
+
+    let path
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    await Category.update({ name, path }, { where: { id } })
+
+    return response.status(200).json()
+  }
+
+  catch(err) {
+    console.log(err)
+  }
 }
 
 export default new CategoryController()
